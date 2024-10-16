648b882c6f6b4fe0e9ef7450c78351d45003cb95
unknown <nguyenluan3062001@gmail.com>
2024-10-16 22:21:32 +0700

    v1

---
+++ thiep-mung-main/thiep-mung-main/config.js
@@ -0,0 +1,13 @@
+const nameGirl = 'Tặng Tất Cả Chị Em Phụ Nữ SMC';
+const giftUrl = '';
+const eventName = 'Chúc Mừng 20-10';
+const titleCard = 'Tặng Tất Cả Chị Em Phụ Nữ SMC';
+const contentCard = 'Nhân ngày Phụ nữ Việt Nam 20/10, xin gửi đến các chị em lời chúc sức khỏe, hạnh phúc và thành công! Mong các chị em luôn rạng rỡ, tự tin và vững bước trên con đường sự nghiệp cũng như trong cuộc sống. Cảm ơn sự cống hiến và nỗ lực không ngừng của các bạn đã góp phần làm nên sự phát triển của công ty. Chúc một ngày 20/10 thật nhiều niềm vui và tràn đầy yêu thương!';
+
+// phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
+// Cần hỗ trợ hãy liên hệ: 
+// Mr-Nam http://facebook.com/nam.nodemy
+// Các bạn muốn học lập trình thì tham gia Nhóm zalo tự học lập trình nhé: https://zalo.me/g/yhdkef092
+const giftImage = '163.png';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
